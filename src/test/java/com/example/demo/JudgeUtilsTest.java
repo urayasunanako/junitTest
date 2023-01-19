dd7615fcd7df022519d17515c6beb88a72cbd8c1
BBC300363 <BBC300363@F01353.cmjgwd.canon-mj.co.jp>
2023-01-19 16:10:05 +0900

    test

--- src/test/java/com/example/demo/JudgeUtilsTest.java
+++ src/test/java/com/example/demo/JudgeUtilsTest.java
@@ -219,4 +219,38 @@ class JudgeUtilsTest {
 		mockTime.tearDown();
 
 	}
+	
+	// ケースNo4
+	// ケース内容：1歳(04/01)でプレイ可能
+	// 誕生時刻(引数)：2000/04/01
+	// 現在時刻：2017/04/01
+	// 合格条件：False
+	@Test
+	public void testIsRegisterdAge_16歳_プレイ可() {
+
+		// 事前処理(Mock)
+		Calendar cal = Calendar.getInstance();
+		cal.clear();
+		cal.set(Calendar.YEAR, 2016);
+		cal.set(Calendar.MONTH, Calendar.APRIL);
+		cal.set(Calendar.DAY_OF_MONTH, 02);
+		Date date = cal.getTime();
+		MockCurrentTime mockTime = new MockCurrentTime(date);
+
+		// 引数
+		Calendar birthcal = Calendar.getInstance();
+		birthcal.clear();
+		birthcal.set(Calendar.YEAR, 2000);
+		birthcal.set(Calendar.MONTH, Calendar.APRIL);
+		birthcal.set(Calendar.DAY_OF_MONTH, 01);
+		Date birthDay = birthcal.getTime();
+
+		// 実行
+		boolean actual = target.isRegisterdAge(birthDay);
+
+		//結果
+		assertFalse(actual);
+		mockTime.tearDown();
+
+	}
 }
